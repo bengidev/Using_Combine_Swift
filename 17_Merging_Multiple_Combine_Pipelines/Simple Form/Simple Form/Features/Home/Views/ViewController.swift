1fab428eb25c69ad2ce09f0eb820687496e40f35
B e n g i <bambang.trd17@gmail.com>
2024-01-04 12:41:39 +0700

    feat: add NavigationController with large titles

---
+++ 17_Merging_Multiple_Combine_Pipelines/Simple Form/Simple Form/Features/Home/Views/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Simple Form
+//
+//  Created by Bambang Tri Rahmat Doni on 04/01/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    // MARK: Initializers
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @available(*, unavailable)
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        
+        fatalError("awakeFromNib() has not been implemented")
+    }
+
+    // MARK: Lifecycles
+    override func loadView() {
+        super.loadView()
+        
+        setupViews()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: Functionalities
+    private func setupViews() -> Void {
+        navigationController?.navigationBar.topItem?.title = "Simple Form"
+        
+        view.backgroundColor = .systemGray6
+    }
+
+}
+
